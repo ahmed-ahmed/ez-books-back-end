38b88ee97ed7502e97f2ed9c89a2b52e68118bf6
Ahmed Ahmed <aahmed@cas.org>
2019-10-07 15:50:26 -0400

    adding swagger

--- src/main/java/io/ezbook/api/ApiApplication.java
+++ src/main/java/io/ezbook/api/ApiApplication.java
@@ -2,14 +2,32 @@ package io.ezbook.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+import static springfox.documentation.builders.PathSelectors.any;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableSwagger2
 public class ApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
+    }
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("io.ezbook.api.controller")).paths(any()).build()
+                .apiInfo(new ApiInfo("Ez books api", "Ez books api", "0.0.1", null, null, null, null, new ArrayList<>()));
     }
 
 }
